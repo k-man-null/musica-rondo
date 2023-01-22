48be860ea988338d2384d6374bd1ffbc651f26da
konchellah <williamoriginal2019@gmail.com>
2023-01-22 21:21:39 +0300

    Gradient Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <GradientLayout 
+    color="green" 
+    subtitle="profile" 
+    title="Bill Konchellah"
+    description="15 public playlists"
+    image="https://avatars.githubusercontent.com/u/74916504?v=4"
+    roundImage>
+      <div>Homepage</div>
+    </GradientLayout>
   )
 }
 
